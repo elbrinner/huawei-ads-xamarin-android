b0df0685be78be32ffb03f29ebdb7475dc2518f4
Elbrinner da silva fernandes <edasilvf@everis.com>
2020-07-27 00:51:08 +0200

    Inicio teste con Xamarin.Forms, no funciona.

---
+++ FormsAdsHuawei/FormsAdsHuawei.Android/Renderers/BannerRenderer.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using FormsAdsHuawei.Controls;
+using FormsAdsHuawei.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Com.Huawei.Hms.Ads;
+using Com.Huawei.Hms.Ads.Banner;
+using Android.Content;
+using Android.Widget;
+
+[assembly: ExportRenderer(typeof(AdBanner), typeof(BannerRenderer))]
+namespace FormsAdsHuawei.Droid.Renderers
+{
+    public class BannerRenderer : ViewRenderer<AdBanner, BannerView>
+    {
+        string _adUnitId = "testw6vs28auh3";
+
+        BannerView _adView;
+
+        private BannerView defaultBannerView;
+
+        private const int REFRESH_TIME = 30;
+
+        public BannerRenderer(Context context) : base(context)
+        {
+
+        }
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AdBanner> e)
+
+        {
+
+            base.OnElementChanged(e);
+
+            if (Control == null)
+
+            {
+
+                CreateNativeControl(e.NewElement, e.NewElement.AdsId, e.NewElement.PersonalizedAds);
+
+                SetNativeControl(_adView);
+
+            }
+
+        }
+
+
+
+        private void CreateNativeControl(AdBanner myMtAdView, string adsId, bool? personalizedAds)
+
+        {
+
+            if (_adView != null)
+
+                return;
+
+
+
+            _adUnitId = !string.IsNullOrEmpty(adsId) ? adsId : "testw6vs28auh3";
+
+
+
+            if (string.IsNullOrEmpty(_adUnitId))
+
+            {
+
+                Console.WriteLine("You must set the adsID before using it");
+
+            }
+
+
+
+            _adView = new BannerView(Context)
+
+            {
+
+                BannerAdSize = BannerAdSize.BannerSize46860,
+
+                AdId = _adUnitId,
+
+                LayoutParameters = new LinearLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent)
+
+            };
+
+
+
+            _adView.AdListener = new AdsListener(Context);
+
+
+
+
+
+            AdParam adParam = new AdParam.Builder().Build();
+
+            _adView.LoadAd(adParam);
+
+
+
+            _adView.SetBannerRefresh(REFRESH_TIME);
+
+
+
+
+
+        }
+
+
+
+        private class AdsListener : AdListener
+
+        {
+
+
+
+            public AdsListener(Context activity)
+
+            {
+
+
+
+            }
+
+            public override void OnAdClicked()
+
+            {
+
+                // Called when a user taps an ad.
+
+                var a = 1;
+
+
+
+            }
+
+            public override void OnAdClosed()
+
+            {
+
+                // Called when an ad is closed.
+
+                var a = 1;
+
+
+
+            }
+
+            public override void OnAdFailed(int errorCode)
+
+            {
+
+                // Called when an ad fails to be loaded.
+
+                var a = 1;
+
+
+
+            }
+
+
+
+            public override void OnAdLeave()
+
+            {
+
+                // Called when a user has left the app.
+
+                var a = 1;
+
+
+
+            }
+
+            public override void OnAdOpened()
+
+            {
+
+                // Called when an ad is opened.
+
+                var a = 1;
+
+
+
+            }
+
+            public override void OnAdLoaded()
+
+            {
+
+                // Called when an ad is loaded successfully.
+
+                var a = 1;
+            }
+        }
+    }
+}
---
+++ FormsAdsHuawei/FormsAdsHuawei/Controls/AdBanner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsAdsHuawei.Controls
+{
+    public class AdBanner : View
+    {
+        public event EventHandler AdsClicked;
+        public event EventHandler AdsClosed;
+        public event EventHandler AdsImpression;
+        public event EventHandler AdsOpened;
+
+        public static readonly BindableProperty AdsIdProperty = BindableProperty.Create("AdsId", typeof(string), typeof(AdBanner));
+
+        public string AdsId
+        {
+            get => (string)GetValue(AdsIdProperty);
+            set => SetValue(AdsIdProperty, value);
+        }
+
+        public static readonly BindableProperty PersonalizedAdsProperty = BindableProperty.Create("PersonalizedAds", typeof(bool), typeof(AdBanner));
+
+        public bool? PersonalizedAds
+        {
+            get => (bool?)GetValue(PersonalizedAdsProperty);
+            set => SetValue(PersonalizedAdsProperty, value);
+        }
+
+        public void AdClicked(object sender, EventArgs e)
+        {
+            AdsClicked?.Invoke(sender, e);
+        }
+
+        public void AdClosed(object sender, EventArgs e)
+        {
+            AdsClosed?.Invoke(sender, e);
+        }
+
+        public void AdImpression(object sender, EventArgs e)
+        {
+            AdsImpression?.Invoke(sender, e);
+        }
+
+        public void AdOpened(object sender, EventArgs e)
+        {
+            AdsOpened?.Invoke(sender, e);
+        }
+    }
+}
